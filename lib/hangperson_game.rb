ef71433994135ef85b339368a6130492bc6654d8
Jonathan Byrne <jbyrne6@uncc.edu>
2017-10-01 04:42:41 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,26 +2,62 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :guesses, :word, :wrong_guesses
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
-  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(letter)
+    fail ArgumentError if letter.nil? || !valid_guess?(letter)
+    letter = letter.downcase
+
+    return false if already_guessed?(letter)
+
+    if word.include?(letter)
+      guesses << letter
+    else
+      wrong_guesses << letter
+    end
+
+    true
+  end
+
+  def check_win_or_lose
+    return :lose if wrong_guesses.length >= 7
+    return :win unless word_with_guesses.include?('-')
+    :play
+  end
+
+  def word_with_guesses
+    formatted_word = ''
+
+    word.split('').each do |letter|
+      formatted_word << (guesses.include?(letter) ? letter : '-')
+    end
+
+    formatted_word
+  end
+
+  private
+
+  def already_guessed?(letter)
+    (guesses + wrong_guesses).include?(letter)
+  end
+
+  def valid_guess?(letter)
+    letter =~ /^[a-z]$/i
   end
 
 end
