b252122d49b93efd7a23d0307cd006f5cc837432
Jonathan Byrne <jbyrne6@uncc.edu>
2017-10-06 15:37:17 +0000

    Fixing redirects v2

--- app.rb
+++ app.rb
@@ -52,8 +52,10 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     if @game.check_win_or_lose == :lose
+      redirect '/lose'
       erb :lose
     elsif @game.check_win_or_lose == :win
+      redirect '/win'
       erb :win
     else
       erb :show # You may change/remove this line
